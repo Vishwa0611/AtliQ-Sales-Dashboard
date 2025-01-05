cfa3da18bb546cf6ee269f9de01c3d386cf2a130
Vishwa0611 <153889527+Vishwa0611@users.noreply.github.com>
2025-01-05 14:11:53 +0530

    Add files via upload

---
+++ AtliQ sales dashboard/Readme.txt
@@ -0,0 +1,21 @@
+Problem Statement:
+
+AtliQ Hardware, a large hardware company with multiple regional offices across India, is facing a decline in sales. Sales Director Pawan Patel struggles to analyze sales trends due to the inefficiency of traditional methods, like managing large, complex Excel sheets from district managers. These sheets are often inaccurate, delayed, and difficult to interpret, making it challenging to gain actionable insights.
+
+Solution Requirements:
+
+Pawan needs a real-time, visual solution to track key metrics like top products, revenue trends, and regional performance. This solution should replace cumbersome Excel files with an intuitive tool to simplify data analysis and support faster decision-making.
+
+Proposed Approach:
+
+  Planning:
+
+   Pawan will meet with the IT Director and Data Analytics team to define goals and success criteria using the AIMS Grid.
+
+  Data Discovery:
+
+   The Data Analytics team will collaborate with IT to access sales records stored in a MySQL database. Tableau will be connected to pull the required data.
+
+  Visualization:
+
+   Tableau dashboards will present interactive and visual insights, enabling better understanding of sales trends and facilitating quicker, informed decisions.
---
+++ AtliQ sales dashboard/SQL query in data set.sql
@@ -0,0 +1,39 @@
+-- to select all the records from the table.
+select * from sales.customers;
+
+-- how many zones exist in the market table.  
+select distinct zone from sales.markets;
+
+-- how many product_type exist in product table.
+select distinct product_type from sales.products;
+
+-- sum of all transactions are in table.
+select count(*) from sales.transactions;
+ 
+-- which type of currency exist in the transcation table.
+select distinct currency from sales.transactions;
+
+# currency
+-- 'INR'  
+# currency
+-- 'INR\r'
+-- its show's me 3 type of currecy. which are INR,USD,INR. when i used distinct keyword after that it shows 2 INR which means they both are differnt. 
+-- after pasting both with copy row(with names). I see the problem . it is a data entry problem which i hadel it later.alter
+
+-- it shows me all the transcations of market_code of Mark001
+select * from sales.transactions where market_code='Mark001'; 
+
+--  total sum of sales_amount of market Mark001 from transcation table.
+select sum(transactions.sales_amount) from sales.transactions where market_code='Mark001';
+
+-- it shows which products are sold in chennai(Mark001) .
+select distinct product_code from sales.transactions where market_code='Mark001';  
+
+-- transaction of USD
+select * from sales.transactions where currency='USD';
+
+-- i selected sales as a default schema
+-- i am joinig two tables transaction and date .  using inner join on order_date and date . and to show of 2020.
+SELECT transactions.*,date.* FROM transactions INNER JOIN date ON transactions.order_date = date.date where date.year= '2020';
+
+ALTER TABLE sales_insight CHANGE COLUMN profit_margin profit DECIMAL(10,2);
