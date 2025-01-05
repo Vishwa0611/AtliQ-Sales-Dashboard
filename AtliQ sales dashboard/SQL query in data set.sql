2f7e7238543d67e0109bb40584476d31947ae4cb
Vishwa0611 <153889527+Vishwa0611@users.noreply.github.com>
2025-01-05 14:12:42 +0530

    SQL query in data set.sql

--- AtliQ sales dashboard/SQL query in data set.sql
+++ AtliQ sales dashboard/SQL query in data set.sql
@@ -18,7 +18,7 @@ select distinct currency from sales.transactions;
 # currency
 -- 'INR\r'
 -- its show's me 3 type of currecy. which are INR,USD,INR. when i used distinct keyword after that it shows 2 INR which means they both are differnt. 
--- after pasting both with copy row(with names). I see the problem . it is a data entry problem which i hadel it later.alter
+-- after pasting both with copy row(with names). I see the problem . it is a data entry problem which i hadle it later.
 
 -- it shows me all the transcations of market_code of Mark001
 select * from sales.transactions where market_code='Mark001'; 
